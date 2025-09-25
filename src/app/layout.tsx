b05546fd6bdeb0fee245b827e3f3f350d360e430
Esepe-digital <digital@esepe.cl>
2025-09-25 12:37:38 -0300

    tgm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,15 @@ export default async function RootLayout({
       {process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' && (
         <GoogleTagManager gtmId="GTM-WL9RHFB" />
       )}
-      { 'GTM-WL9RHFB' }
+      { 
+      '<!-- Google Tag Manager -->'
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PLTSMZQ2');</script>
+      '<!-- End Google Tag Manager -->'
+      }
       {process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' && (
         <GoogleAnalytics gaId="AW-10877400073" />
       )}
