fd4ad2a84a04b82134b7c30baf156cc0f957d311
Esepe-digital <digital@esepe.cl>
2025-09-25 12:39:53 -0300

    tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout({
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PLTSMZQ2');</script>
+        })(window,document,'script','dataLayer','GTM-WL9RHFB');</script>
       '<!-- End Google Tag Manager -->'
       }
       {process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' && (
